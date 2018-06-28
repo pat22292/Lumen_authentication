1a38da0838cb49cf69eb0bf6d0bb558f8c80417b
madlangawapat22@gmail.com <madlangawapat22@gmail.com>
2018-06-28 11:16:10 +0800

    changed someting

--- config/services.php
+++ config/services.php
@@ -5,5 +5,6 @@ return [
         'login_endpoint' => env('PASSPORT_LOGIN_ENDPOINT'),
         'client_id' => env('PASSPORT_CLIENT_ID'),
         'client_secret' => env('PASSPORT_CLIENT_SECRET'),
+        'do_not_use' => env('DO_NOT_USE')
     ]
 ];
